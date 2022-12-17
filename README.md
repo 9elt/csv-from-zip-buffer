8765d9dcc454300c60505be31779504dee2e198f
9elt <nelt.primary@gmail.com>
2022-12-17 23:03:37 +0100

    error handling

--- README.md
+++ README.md
@@ -1,17 +1,23 @@
 # csv from zip buffer
-get csv data from a zip file buffer.
+get csv data from a zip file buffer without creating temporary files.
 
 > Requires the [requests](https://pypi.org/project/requests/) module.
 
 ## example
 
 ```python
-my_data = csv_from_zip_buffer(Settings(
+
+res = RequestZip(
     ",", # csv delimiter
     "https://mysite.com/myfile.zip", # zip file url
     "target_file_name.csv" # targeted file name inside the zip archive
-))
+).get()
 
-print(my_data[0][0])
-# output: my first clumn title
+if isinstance(res, RequestZipError):
+    print(f"{res.code} - {res.msg}")
+else:
+    print(res[0][0])
+
+# expected output: my first column heading
+
 ```
--- main.py
+++ main.py
@@ -1,44 +1,68 @@
 import requests
 import zipfile
-import io
+from io import BytesIO 
 import csv
 
 
-#   requests a zip file to a url and
-#   returns the utf-8 decoded targeted file
-def request_zip_file(settings):
-    response = requests.get(settings.url)
-    if response.status_code != 200:
-        return response.status_code
-
-    zip_archive = zipfile.ZipFile(io.BytesIO(response.content))
-
-    file_bytes = zip_archive.read(settings.file)
-    return file_bytes.decode("utf-8", errors='ignore')
+# Error class
+class RequestZipError:
+    def __init__(self, code, msg):
+        self.code = code
+        self.msg = msg
 
 
-#   opens a csv file from a zip file buffer and
-#   returns the parsed data as a 2d array
-def csv_from_zip_buffer(settings):
-    data = []
-    rows = request_zip_file(settings).split("\n")
-    for cols in rows: data.append(cols.split(settings.delimiter))
-    return data
-
-
-#   csv_from_zip_buffer() settings
-class Settings:
+class RequestZip:
     def __init__(self, delimiter, target_url, target_file):
         self.delimiter = delimiter
         self.url = target_url
         self.file = target_file
 
+    #   opens a csv file from a zip file buffer and
+    #   returns the a 2d array of the parsed data 
+    def get(self):
+        res = self.request_zip_file()
+        if isinstance(res, RequestZipError):
+            return res
+
+        data = []
+        rows = res.split("\n")
+        for cols in rows: data.append(cols.split(self.delimiter))
+        return data
+
+    #   requests a zip file to a url and
+    #   returns the utf-8 decoded targeted file
+    def request_zip_file(self):
+        try:
+            response = requests.get(self.url)
+        except:
+            return RequestZipError(500, "failed request")
+
+        if response.status_code != 200:
+            return RequestZipError(response.status_code,"failed request with status code")
+
+        try:
+            zip_archive = zipfile.ZipFile(BytesIO(response.content))
+        except:
+            return RequestZipError(500, "failed opening zip file")
+
+        try:
+            file_bytes = zip_archive.read(self.file)
+        except:
+            return RequestZipError(500, "failed reading zip file")
+
+        return file_bytes.decode("utf-8", errors='ignore')
+
 
 #   example
-my_data = csv_from_zip_buffer(Settings(
-    ",",
-    "https://mysite.com/myfile.zip",
-    "target_file_name.csv"
-))
+res = RequestZip(
+    ",", # csv delimiter
+    "https://mysite.com/myfile.zip", # zip file url
+    "target_file_name.csv" # targeted file name inside the zip archive
+).get()
 
-print(my_data[0][0])
+if isinstance(res, RequestZipError):
+    print(f"{res.code} - {res.msg}")
+else:
+    print(res[0][0])
+
+# expected output: my first column heading
